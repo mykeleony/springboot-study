0656e564b903cc7e7d2c549591d30b48895540c7
myke amorim <myke.amorim@usp.br>
2023-02-23 11:24:55 -0300

    First commit

---
+++ src/main/java/io/github/mykeleony/SalesApplication.java
@@ -0,0 +1,10 @@
+package io.github.mykeleony;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesApplication {
+    public static void main(String[] args) {
+
+    }
+}

0583765ddd89b69853b23da57d6d311dd3f34dd5
myke amorim <myke.amorim@usp.br>
2023-02-23 11:27:07 -0300

    Created main class

--- src/main/java/io/github/mykeleony/SalesApplication.java
+++ src/main/java/io/github/mykeleony/SalesApplication.java
@@ -1,10 +1,13 @@
 package io.github.mykeleony;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SalesApplication {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        SpringApplication.run(SalesApplication.class, args);
     }
+
 }

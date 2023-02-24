4e2553c0f116308ddbbb64176abd49b2c355d3a8
myke amorim <myke.amorim@usp.br>
2023-02-24 12:32:47 -0300

    Limited configuration to development env

--- src/main/java/io/github/mykeleony/ConfigurationExample.java
+++ src/main/java/io/github/mykeleony/ConfigurationExample.java
@@ -1,12 +1,20 @@
 package io.github.mykeleony;
 
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("development")
 public class ConfigurationExample {
 
-
+    @Bean
+    public CommandLineRunner execute() {
+        return args -> {
+            System.out.println("Running development configuration!");
+        };
+    }
 
 }
--- src/main/java/io/github/mykeleony/SalesApplication.java
+++ src/main/java/io/github/mykeleony/SalesApplication.java
@@ -1,7 +1,5 @@
 package io.github.mykeleony;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

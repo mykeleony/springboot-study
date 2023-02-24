1a4b92fead31e02b2bec40503fc527ba7177cdb3
myke amorim <myke.amorim@usp.br>
2023-02-24 12:04:33 -0300

    Created default application name property

--- src/main/java/io/github/mykeleony/ConfigurationExample.java
+++ src/main/java/io/github/mykeleony/ConfigurationExample.java
@@ -7,9 +7,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ConfigurationExample {
 
-    @Bean(name = "applicationName")
-    public String applicationName() {
-        return "Sales application";
-    }
+
 
 }

cd71a3dd4fb1744bc0a7ad63201656574e3bf7ac
myke amorim <myke.amorim@usp.br>
2023-02-27 18:34:41 -0300

    Created custom annotation

--- src/main/java/io/github/mykeleony/ConfigurationExample.java
+++ src/main/java/io/github/mykeleony/ConfigurationExample.java
@@ -1,13 +1,14 @@
 package io.github.mykeleony;
 
 
+import io.github.mykeleony.annotation.Development;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("development")
+@Development
 public class ConfigurationExample {
 
     @Bean
---
+++ src/main/java/io/github/mykeleony/annotation/Development.java
@@ -0,0 +1,16 @@
+package io.github.mykeleony.annotation;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)   // Classes only
+@Retention(RetentionPolicy.RUNTIME)
+@Configuration
+@Profile("development")
+public @interface Development {
+}

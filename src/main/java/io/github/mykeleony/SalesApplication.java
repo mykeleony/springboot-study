bb0e00cdb018060095964afb88b902fa099d9e2e
myke amorim <myke.amorim@usp.br>
2023-02-27 19:30:16 -0300

    Reset main class

---
+++ src/main/java/io/github/mykeleony/SalesApplication.java
@@ -0,0 +1,14 @@
+package io.github.mykeleony;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalesApplication.class, args);
+    }
+
+
+}

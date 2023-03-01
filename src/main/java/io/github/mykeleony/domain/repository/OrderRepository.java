cbbb38b00e670babea99d515356d61660a75b393
myke amorim <myke.amorim@usp.br>
2023-03-01 20:51:19 -0300

    Added find by customer query method

--- src/main/java/io/github/mykeleony/domain/repository/OrderRepository.java
+++ src/main/java/io/github/mykeleony/domain/repository/OrderRepository.java
@@ -1,7 +1,14 @@
 package io.github.mykeleony.domain.repository;
 
+import io.github.mykeleony.domain.model.Customer;
 import io.github.mykeleony.domain.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Set;
+
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    Set<Order> findByCustomer(Customer customer);
+
 }

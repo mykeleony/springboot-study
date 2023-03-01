366fd6c7b4bd70b77110ab3984bca2863d4acd3f
myke amorim <myke.amorim@usp.br>
2023-03-01 20:40:00 -0300

    Created field containing all orders

--- src/main/java/io/github/mykeleony/domain/model/Customer.java
+++ src/main/java/io/github/mykeleony/domain/model/Customer.java
@@ -3,11 +3,13 @@ package io.github.mykeleony.domain.model;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.util.Set;
 
 @Getter
 @Setter
+@ToString
 @Entity(name = "customers")
 public class Customer {
 
@@ -17,7 +19,7 @@ public class Customer {
 
     private String name;
 
-    @OneToMany(mappedBy = "customer")
+    @OneToMany(mappedBy = "customer", fetch = FetchType.EAGER)
     private Set<Order> orders;
 
 }

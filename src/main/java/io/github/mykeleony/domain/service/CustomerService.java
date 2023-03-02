ce5aadbd6d6007a13baaf09e445dc94e7d554d51
myke amorim <myke.amorim@usp.br>
2023-03-02 14:29:54 -0300

    Added save method

--- src/main/java/io/github/mykeleony/domain/service/CustomerService.java
+++ src/main/java/io/github/mykeleony/domain/service/CustomerService.java
@@ -5,6 +5,7 @@ import io.github.mykeleony.domain.repository.CustomerRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -22,6 +23,11 @@ public class CustomerService {
     public ResponseEntity<Customer> findById(Long id) {
         return customerRepository.findById(id).map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
+    }
+
+    @Transactional
+    public Customer save(Customer customer) {
+        return customerRepository.save(customer);
     }
 
 //    public List<Customer> searchCustomers2(String name) {

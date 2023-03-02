c15f351b56d704e63a5b58a056d1f88eed99eb60
myke amorim <myke.amorim@usp.br>
2023-03-01 21:07:51 -0300

    Created customer REST controller

---
+++ src/main/java/io/github/mykeleony/api/controller/CustomerController.java
@@ -0,0 +1,23 @@
+package io.github.mykeleony.api.controller;
+
+import io.github.mykeleony.domain.repository.CustomerRepository;
+import io.github.mykeleony.domain.service.CustomerService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/sales")
+public class CustomerController {
+
+    private CustomerService customerService;
+
+    @GetMapping("/hello/{name}")
+    public String helloClient(@PathVariable String name) {
+        return String.format("Hello, %s! Welcome to Myke's REST API!", name);
+    }
+
+}

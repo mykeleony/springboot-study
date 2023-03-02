c54ae67ae9c48d860cae012eac1647698e327ff0
myke amorim <myke.amorim@usp.br>
2023-03-02 11:25:28 -0300

    Added get by id method

--- src/main/java/io/github/mykeleony/domain/service/CustomerService.java
+++ src/main/java/io/github/mykeleony/domain/service/CustomerService.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @AllArgsConstructor
 @Service
@@ -15,6 +16,10 @@ public class CustomerService {
 
     public List<Customer> findByName(String name) {
         return customerRepository.findCustomersByNameLike(name);
+    }
+
+    public Optional<Customer> findById(Long id) {
+        return customerRepository.findById(id);
     }
 
 //    public List<Customer> searchCustomers2(String name) {

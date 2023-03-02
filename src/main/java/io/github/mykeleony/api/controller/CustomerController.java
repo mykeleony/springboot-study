1b7f1d512d0887503586d37e2601a13380b3f199
myke amorim <myke.amorim@usp.br>
2023-03-02 11:25:11 -0300

    Added get endpoint using @RequestMapping

--- src/main/java/io/github/mykeleony/api/controller/CustomerController.java
+++ src/main/java/io/github/mykeleony/api/controller/CustomerController.java
@@ -1,12 +1,8 @@
 package io.github.mykeleony.api.controller;
 
-import io.github.mykeleony.domain.repository.CustomerRepository;
 import io.github.mykeleony.domain.service.CustomerService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @AllArgsConstructor
 @RestController
@@ -16,8 +12,14 @@ public class CustomerController {
     private CustomerService customerService;
 
     @GetMapping({"/hello/{name}", "/hey/{name}"})
-    public String helloClient(@PathVariable String name) {
+    public String helloCustomer(@PathVariable String name) {
         return String.format("Hello, %s! Welcome to Myke's REST API!", name);
+    }
+
+    @RequestMapping(value = "/helloRequest/{name}", method = RequestMethod.GET)
+    @ResponseBody
+    public String helloRequestCustomer(@PathVariable String name) {
+        return String.format("Hello, %s that is using @RequestMapping! Welcome to Myke's REST API!", name);
     }
 
 }

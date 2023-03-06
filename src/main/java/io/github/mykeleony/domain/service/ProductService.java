0532a6d4f937d84696556a44ebdbf437a062adc4
myke amorim <myke.amorim@usp.br>
2023-03-06 17:13:02 -0300

    Implemented filtered search

--- src/main/java/io/github/mykeleony/domain/service/ProductService.java
+++ src/main/java/io/github/mykeleony/domain/service/ProductService.java
@@ -4,11 +4,15 @@ import io.github.mykeleony.domain.model.Customer;
 import io.github.mykeleony.domain.model.Product;
 import io.github.mykeleony.domain.repository.ProductRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @AllArgsConstructor
 @Service
@@ -19,6 +23,16 @@ public class ProductService {
     public ResponseEntity<Product> get(Long id) {
         return productRepository.findById(id).map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
+    }
+
+    public List<Product> findFiltered(Product product) {
+        ExampleMatcher exampleMatcher = ExampleMatcher.matching()
+                .withIgnoreCase()
+                .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING);
+
+        Example<Product> example = Example.of(product, exampleMatcher);
+
+        return productRepository.findAll(example);
     }
 
     @Transactional

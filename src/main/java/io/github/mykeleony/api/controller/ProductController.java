8273cc73673893f99e06d034e4fa6dc2a6c6a43b
myke amorim <myke.amorim@usp.br>
2023-03-06 17:13:12 -0300

    Added listing endpoint

--- src/main/java/io/github/mykeleony/api/controller/ProductController.java
+++ src/main/java/io/github/mykeleony/api/controller/ProductController.java
@@ -4,10 +4,11 @@ import io.github.mykeleony.domain.model.Product;
 import io.github.mykeleony.domain.repository.ProductRepository;
 import io.github.mykeleony.domain.service.ProductService;
 import lombok.AllArgsConstructor;
-import org.springframework.boot.env.RandomValuePropertySourceEnvironmentPostProcessor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @AllArgsConstructor
 @RestController
@@ -20,6 +21,11 @@ public class ProductController {
     @GetMapping("/{id}")
     public ResponseEntity<Product> get(@PathVariable Long id) {
         return productService.get(id);
+    }
+
+    @GetMapping
+    public List<Product> find(Product product) {
+        return productService.findFiltered(product);
     }
 
     @PostMapping
